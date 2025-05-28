702a1a9c4ebc9181e93728b76f96793bcb514c73
NhuqyGit <kathonflag@gmail.com>
2025-05-28 13:45:21 +0700

    fix: change host swagger-ui

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -202,7 +202,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "go-backend-rc59.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "GoldenOwl Student Score API",
