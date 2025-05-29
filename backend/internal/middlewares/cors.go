50b8a8bc4afab53d1e55e4d0cf6b2e4def50d690
NhuqyGit <kathonflag@gmail.com>
2025-05-30 00:27:43 +0700

    add: url fe in cors

--- backend/internal/middlewares/cors.go
+++ backend/internal/middlewares/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://golden-owl-gscore.netlify.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
