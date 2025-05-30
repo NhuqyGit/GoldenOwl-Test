b06d6d6748a4babe04eedec8fb2928f80a78148c
NhuqyGit <kathonflag@gmail.com>
2025-05-30 10:55:10 +0700

    update: responsive header, menu

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -5,63 +5,106 @@ import { RxDashboard } from "react-icons/rx";
 import { IoSearchOutline } from "react-icons/io5";
 import { FaRegChartBar } from "react-icons/fa";
 import { CgDigitalocean } from "react-icons/cg";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 import "./Menu.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const isT1 = windowWidth > 780;
+    const isT2 = windowWidth <= 580 && windowWidth > 430;
+    const isT3 = windowWidth <= 430;
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     const handleOpenMenu = () => {
-        setIsOpen(!isOpen);
+        if (isT3) {
+            setIsOpen(!isOpen); // dọc
+        } else if (isT1) {
+            setIsOpen(!isOpen); // ngang
+        }
+        // T2 không cho mở
     };
 
     const handleNavClick = () => {
-        if (!isOpen) setIsOpen(true);
+        if (isT3) {
+            setIsOpen(false); // đóng menu sau khi chọn item ở T3
+        }
+    };
+
+    const handNavDoubleClick = () => {
+        if (isT1 && !isOpen) {
+            setIsOpen(true); // ở T1 thì mở nếu đang đóng
+        }
     };
 
     return (
-        <div className={`menu-container ${isOpen ? "open" : "closed"}`}>
-            <div className="menu-header">
-                <a href="/" className="logo">
-                    <CgDigitalocean color="var(--yellow)" size={30} />
-                    {isOpen && <h1>G-Scores</h1>}
-                </a>
-                {isOpen && (
-                    <div className="sidebar-icon" onClick={handleOpenMenu}>
-                        <BsLayoutSidebarInset color="white" size={18} />
-                    </div>
-                )}
+        <>
+            {isT3 && !isOpen && (
+                <div className="mobile-menu-button" onClick={handleOpenMenu}>
+                    <HiOutlineMenuAlt3 size={28} color="var(--black)" />
+                </div>
+            )}
+
+            <div
+                className={`menu-container 
+                    ${isOpen ? "open" : "closed"} 
+                    ${isT3 ? "mobile" : ""} `}
+            >
+                <div className="menu-header">
+                    <a href="/" className="logo">
+                        <CgDigitalocean color="var(--yellow)" size={30} />
+                        {(isOpen || isT3) && <h1>G-Scores</h1>}
+                    </a>
+                    {isOpen && (
+                        <div className="sidebar-icon" onClick={handleOpenMenu}>
+                            <BsLayoutSidebarInset color="white" size={18} />
+                        </div>
+                    )}
+                </div>
+
+                <nav>
+                    <NavLink
+                        to="/"
+                        onDoubleClick={handNavDoubleClick}
+                        onClick={handleNavClick}
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                    >
+                        <RxDashboard color="white" size={18} />
+                        {(isOpen || isT3) && <span>Dashboard</span>}
+                    </NavLink>
+
+                    <NavLink
+                        to="/search-page"
+                        onDoubleClick={handNavDoubleClick}
+                        onClick={handleNavClick}
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                    >
+                        <IoSearchOutline color="white" size={18} />
+                        {(isOpen || isT3) && <span>Search Scores</span>}
+                    </NavLink>
+
+                    <NavLink
+                        to="/report"
+                        onDoubleClick={handNavDoubleClick}
+                        onClick={handleNavClick}
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                    >
+                        <FaRegChartBar color="white" size={18} />
+                        {(isOpen || isT3) && <span>Reports</span>}
+                    </NavLink>
+                </nav>
             </div>
-            <nav>
-                <NavLink
-                    to="/"
-                    onDoubleClick={handleNavClick}
-                    className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                    <RxDashboard color="white" size={18} />
-                    {isOpen && <span>Dashboard</span>}
-                </NavLink>
-
-                <NavLink
-                    to="/search-page"
-                    onDoubleClick={handleNavClick}
-                    className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                    <IoSearchOutline color="white" size={18} />
-                    {isOpen && <span>Search Scores</span>}
-                </NavLink>
-
-                <NavLink
-                    to="/report"
-                    onDoubleClick={handleNavClick}
-                    className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                    <FaRegChartBar color="white" size={18} />
-                    {isOpen && <span>Reports</span>}
-                </NavLink>
-            </nav>
-        </div>
+        </>
     );
 };
 
